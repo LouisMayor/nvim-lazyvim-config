030d2da451bc46e263f24b1287a09aa2fa055ecb
Louis Mayor <louismayor1@gmail.com>
2025-09-20 19:37:33 +0100

    added keymaps to move between next and previous buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,5 @@ local set = vim.keymap.set
 -- local del = vim.keymap.del
 
 -- set("n", "<A-\\>", "<cmd>Telescope find_files<cr>", { noremap = true })
+set("n", "<C-x>", "<cmd>:bnext<cr>", { noremap = true })
+set("n", "<C-z>", "<cmd>:bprev<cr>", { noremap = true })
